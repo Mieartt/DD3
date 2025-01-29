da64f71fb648e984673bed8544bbd8ad2f4de079
Mieartt <ard.iheb@gmail.com>
2025-01-29 12:36:08 +0100

    iheb rana zedna architecture (derna jel fi ch3ar)

--- ABDOU.vhd
+++ ABDOU.vhd
@@ -1,6 +1,12 @@
 library ieee;
 use ieee.std_logic_116.all;
 entity abdou is
-port ( wednin, khcham : in std_logic;
-        fom           : out std_logic);
+port ( wednin, khcham   : in std_logic;
+        fom             : out std_logic);
 end entity;
+architecture behavior of abdou is
+signal sch3ar   : std_logic_vector (2 downto 0);
+signal jel : std_logic;
+begin
+ch3ar <= jel;
+end architecture;
